248d53acdc01219d3295eca6205050f5568c6d6a
Peh099 <70337717+Peh099@users.noreply.github.com>
2021-10-06 12:41:00 -0300

    reverse app.py

--- app/app.py
+++ app/app.py
@@ -4,17 +4,13 @@ from flask_migrate import Migrate, migrate
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
 
-def minimal_app(config_class):
 app = Flask(__name__)
-app.config.from_object('config')
-return app
 
-def create_app(config_class=None):
-app = minimal_app(config.DevConfig if config_class is None else config_class)
+app.config.from_object('config')
+
 api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
-return app
 
 from .views import usuario_view, sala_view, pergunta_view
