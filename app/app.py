6923d8719da1122718b4f2dfa1f43f3e0d14e6ae
Peh099 <70337717+Peh099@users.noreply.github.com>
2021-10-06 12:28:23 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,32 +4,17 @@ from flask_migrate import Migrate, migrate
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
 
+def minimal_app(config_class):
 app = Flask(__name__)
-
 app.config.from_object('config')
+return app
 
+def create_app(config_class=None):
+app = minimal_app(config.DevConfig if config_class is None else config_class)
 api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
-
-def minimal_app(config_class):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-    return app
-
-
-def create_app(config_class=None):
-    app = minimal_app(config.DevConfig if config_class is None else config_class)
-    database.init_app(app)
-    migrate.init_app(app)
-    controller.init_app(app)
-    auth.init_app(app)
-    cors.init_app(app)
-    schemas.init_app(app)
-    seed.init_app(app)
-    email.init_app(app)
-    swagger.init_app(app)
-    return app
+return app
 
 from .views import usuario_view, sala_view, pergunta_view
